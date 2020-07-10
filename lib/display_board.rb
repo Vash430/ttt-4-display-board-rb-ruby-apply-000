eb4b93c442ca176ed5612a1ba4a954d030954c10
essential-form-0157 <vasmith430@gmail.com>
2020-07-10 20:44:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,2 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
-  cell = "   "
-  separator = "|"
-  lines = "-----------"
-
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
-  puts "#{lines}"
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
-  puts "#{lines}"
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
-end
